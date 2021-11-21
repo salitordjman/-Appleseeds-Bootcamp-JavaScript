796fd56996748c211649797ae69b4f797e20210a
salitordjman <salitorgi@gmail.com>
2021-11-21 09:45:39 +0200

    21.11.21 09:45

--- solutions_Exercises/week4/17.1-closures.js
+++ solutions_Exercises/week4/17.1-closures.js
@@ -1,15 +1,21 @@
 //!11111111111111111111111111-----------------------
 
-// var b = 1;
-// function someFunction(number) {
-// function otherFunction(input) {
-// return b;
-// }
-// b = 5;
-// return otherFunction;
-// }
-// var firstResult = someFunction(9);
-// var result = firstResult(2);
+var b = 1;
+console.log(b);
+function someFunction(number) {
+  console.log(number);
+  function otherFunction(input) {
+    console.log(input);
+    console.log(b);
+    return b;
+  }
+  b = 5;
+  console.log(b);
+  return otherFunction;
+}
+var firstResult = someFunction(9);
+var result = firstResult(2);
+console.log(b);
 
 // //5- someFunction (9) starts and firstResult (2) calls otherFunction is returns B = 5 (because B = 5 is above the reading of the functions) and then returns otherFunction; Returns the B = 5
 
@@ -34,14 +40,17 @@
 //!3333333333333333333333333333333-----------------------
 
 
-let i;
-for (i = 0; i < 3; i++) {
-const log = () => {
-console.log(i);
-}
-setTimeout(log, 100);
-}
+// let i;
+// for (i = 0; i < 3; i++) {
+// const log = () => {
+// console.log(i);
+// }
+// setTimeout(log, 100);
+// // log()
+// // log()
+// // log()
+// }
 
-//3,3,3
+// //3,3,3
 
 
