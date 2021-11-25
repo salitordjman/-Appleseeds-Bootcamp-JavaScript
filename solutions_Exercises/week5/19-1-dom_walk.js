ac74764fec5a60b4e7999dd9f958817a3e98389e
salitordjman <salitorgi@gmail.com>
2021-11-25 08:36:16 +0200

    25.11.21 08:37

---
+++ solutions_Exercises/week5/19-1-dom_walk.js
@@ -0,0 +1,50 @@
+//! 11111111111111111111111111111111111111111111111111111111111111
+
+// const uls =document.querySelectorAll("ul");
+// const liNew =uls[1].querySelector("li:nth-child(2)");
+// liNew.innerText="main title"
+const liStartHere =document.querySelector('.start-here')
+liStartHere.innerText="main title"
+
+//!222222222222222222222222222222222222222222222222222222222222222
+// const extraLi = document.createElement('li');
+// extraLi.innerText="sub title 4"
+// const uls = document.querySelectorAll("ul");
+// uls[1].append(extraLi)
+
+// uls[1].appendChild(extraLi)
+
+
+const extraLi = document.createElement('li');
+extraLi.innerText="sub title 4"
+const uls = document.querySelectorAll("ul");
+const firstLi = uls[1].querySelector("li:last-child");
+firstLi.insertAdjacentElement('afterend',extraLi)
+
+
+
+//!3333333333333333333333333333333333333333333333333333333333333333
+
+// const liS =document.querySelector("li");
+// liS[7].remove()
+
+const liS =document.querySelector("ul li:last-child");
+liS.remove()
+
+// console.dir(lastLiUl1);
+
+
+//!444444444444444444444444444444444444444444444444444444444444444444
+const title=document.querySelector("title");
+title.innerText='Master Of The Dom'
+// console.dir(title);
+
+
+//!55555555555555555555555555555555555555555555555555555555555555555
+
+const p=document.querySelector("p");
+p.innerText='bla bla blalalala'
+
+
+
+
