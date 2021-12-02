e8aa7cd8f1696f8d7ae97e2ef99aa68f817a690a
salitordjman <salitorgi@gmail.com>
2021-12-02 21:16:48 +0200

    srart1

--- solutions_Exercises/week6/endweek/snake/snake.js
+++ solutions_Exercises/week6/endweek/snake/snake.js
@@ -18,6 +18,7 @@ export function update() {
 
 export function draw(gameBoard) {
   snakeBody.forEach(segment => {
+    log (x)
     const snakeElement = document.createElement('div')
     snakeElement.style.gridRowStart = segment.y
     snakeElement.style.gridColumnStart = segment.x
